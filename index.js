1d4f4dabb1d18bb2a56a0ed54c3a66b90ec87c7a
Stephen Schloesser <sschloesser2@gmail.com>
2020-01-14 17:10:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   let arr = []
   
   for(var i=0; i<musicians.length; i++){
-    arr.push("${musicians[i]} plays ${instruments[i]}")
+    arr.push('${musicians[i]} plays ${instruments[i]}')
   }
   return arr
 }
